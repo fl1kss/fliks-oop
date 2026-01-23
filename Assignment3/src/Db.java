e2e9bcb8ca9af99a8aa6dd78b41af1803863247c
fl1kss <146920702+fl1kss@users.noreply.github.com>
2026-01-23 09:37:46 +0500

    Update Db.java

--- Assignment3/src/Db.java
+++ Assignment3/src/Db.java
@@ -3,9 +3,10 @@ import java.sql.*;
 public class Db {
     private static final String DB_URL = "jdbc:postgresql://localhost:5432/social_media_db";
     private static final String DB_USER = "postgres";
-    private static final String DB_PASSWORD = "YOUR_PASSWORD"; // поменяй
+    private static final String DB_PASSWORD = "12345678"; // поменяй
 
     public Connection connect() throws SQLException {
         return DriverManager.getConnection(DB_URL, DB_USER, DB_PASSWORD);
     }
 }
+
